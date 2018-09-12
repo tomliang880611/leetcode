e3507b5b99e3d96281db22dcf4e92fc13c9a000f
梁凯 <liangkai@MacBook-Pro-2018.local>
2018-09-12 22:32:15 +0800

    add comments and test cases for question 34th
    find first and last postion in a sorted array

--- p34_find_first_and_last_in_sorted_array_test.go
+++ p34_find_first_and_last_in_sorted_array_test.go
@@ -1,5 +1,15 @@
 package leetcode
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// The idea was to identify the target first by using 1/2, the expand the
+// the range
+
+// searchRange was the method that return the range of the target
 func searchRange(nums []int, target int) []int {
 	if len(nums) == 0 {
 		return []int{-1, -1}
@@ -35,4 +45,35 @@ func searchRange(nums []int, target int) []int {
 
 	return []int{first, last}
 
+}
+
+func TestSearchRange(t *testing.T) {
+	assert := assert.New(t)
+	var input []int
+	var expect []int
+	var target int
+
+	// test case 1
+	input = []int{1, 2, 3, 4, 4, 5}
+	target = 4
+	expect = []int{3, 4}
+
+	r := searchRange(input, target)
+	assert.Equal(expect, r)
+
+	// test case 2
+	input = []int{1, 2, 3, 4, 4, 5}
+	target = -1
+	expect = []int{-1, -1}
+
+	r = searchRange(input, target)
+	assert.Equal(expect, r)
+
+	// test case 3
+	input = []int{1}
+	target = 1
+	expect = []int{0, 0}
+
+	r = searchRange(input, target)
+	assert.Equal(expect, r)
 }
